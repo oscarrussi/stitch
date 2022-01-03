c8f236bb92013763196d73f1b02bad7bffc194eb
andresporras3423 <oscarrussi@outlook.com>
2022-01-03 16:21:21 -0500

    add tests

---
+++ spark_tutorial/tests/practice1.py
@@ -0,0 +1,20 @@
+from datetime import date
+from pyspark.sql import Row
+from pyspark.sql.session import SparkSession
+from pyspark import SparkContext
+
+sc = SparkContext("local", "First App")
+spark = SparkSession(sc)
+
+Record = Row("country", "utm_campaign", "airtime_in_minutes", "start_date", "end_date")
+
+# Create a tuple of records
+data = (
+  Record("USA", "DiapersFirst", 28, date(2017, 1, 20), date(2017, 1, 27)),
+  Record("Germany", "WindelKind", 31, date(2017, 1, 25), None),
+  Record("India", "CloseToCloth", 32, date(2017, 1, 25), date(2017, 2, 2))
+)
+
+# Create a DataFrame from these records
+frame = spark.createDataFrame(data)
+frame.show()
---
+++ spark_tutorial/tests/test_practice2.py
@@ -0,0 +1,5 @@
+def test_calculate_unit_price_in_euros():
+    assert True
+
+def test_2_equals_3():
+    assert 2 == 3
