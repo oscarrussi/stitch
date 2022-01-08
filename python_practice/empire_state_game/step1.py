4ec1d27923e11cf62bae53e03172a25c39e42fba
andresporras3423 <oscarrussi@outlook.com>
2022-01-08 17:27:13 -0500

    add empire state code

---
+++ python_practice/empire_state_game/step1.py
@@ -0,0 +1,9 @@
+# to generate always the same random number use always the same seed
+# Import numpy as np
+import numpy as np
+
+# Set the seed
+np.random.seed(123)
+
+# Generate and print random float
+print(np.random.rand())
