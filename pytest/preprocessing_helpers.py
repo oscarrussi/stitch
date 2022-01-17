339a7934e0812ffb4ea1a6830fca0268996ce1f8
andresporras3423 <oscarrussi@outlook.com>
2022-01-17 17:08:48 -0500

    complete second section of pytest tutorial

--- pytest/preprocessing_helpers.py
+++ pytest/preprocessing_helpers.py
@@ -1,25 +1,37 @@
+import re
+
 def convert_to_int(number):
-  number = number.replace(",", "")
+  if re.search("^(0|[1-9][0-9]{0,2}(,[0-9]{3})*)$", number) is None:
+    return None
+  number=number.replace(",", "")
   return int(number)
+
+def convert_to_int_alternative(integer_string_with_commas):
+    comma_separated_parts = integer_string_with_commas.split(",")
+    for i in range(len(comma_separated_parts)):
+        # Write an if statement for checking missing commas
+        if len(comma_separated_parts[i]) > 3:
+            return None
+        # Write the if statement for incorrectly placed commas
+        if i != 0 and len(comma_separated_parts[i]) != 3:
+            return None
+    integer_string_without_commas = "".join(comma_separated_parts)
+    try:
+        return int(integer_string_without_commas)
+    # Fill in with a ValueError
+    except ValueError:
+        return None
 
 # print(convert_to_int("2,081"))
 def row_to_list(values):
-  values = values.replace(",", ".")
+  
   values = values.replace("\n", "")
   arr = values.split("\t")
   if len(arr)!=2:
     return None
   for x in arr:
-    if is_number(x)==False:
+    if re.search("^(0|[1-9][0-9]{0,2}(,[0-9]{3})*)(\\.[0-9]+)?$", x) is None:
       return None
   return arr
 
-def is_number(n):
-    try:
-        float(n)
-        return True
-    except ValueError:
-        return False
-
-
-print(row_to_list("3\t34,5\n"))
+# print(row_to_list("1,059\t186,606\n"))
